0e23aedaa266c6ca3b69f3771117c1364b00ef65
ni-romero <luz.nicole.maria@gmail.com>
2020-08-11 11:37:50 -0300

    layout primeros pasos

--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -1,13 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
 
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Healmet, { Helmet } from 'react-helmet'
 
 import Header from "./header"
 import "./layout.css"
@@ -25,20 +20,28 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          width: '50%',
           padding: `0 1.0875rem 1.45rem`,
+          backgroundColor: 'white',
+          opacity: '0.8',
+          left: '0',
+          textAlign: 'center'
+
         }}
       >
+
+
+
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+
       </div>
     </>
   )
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,22 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Image from "../components/image"
+
 
 const IndexPage = () => (
+  <div style={{
+    backgroundImage: 'url(https://images.unsplash.com/photo-1487611459768-bd414656ea10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)',
+    widht: '100%',
+    height: '100vh'
+  }}>
   <Layout>
-    <SEO title="Home" />
-    <h1>Prevengase</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    
+    <h1 style={{
+      textAlign: 'center',
+      color: 'black',
+      
+    }}>Informe sobre personas, empresas, automotor y celulares</h1>
+
+<h4>Informes que te ayudaran a tomar mejores decisiones.</h4>
+<i class="fas fa-laugh-wink fa-7x"></i>
+<h3 style={{
+  color: 'red'
+}}>RÁPIDO, FÁCIL Y EN SIMPLES PASOS!</h3>
   </Layout>
+  </div>
 )
 
 export default IndexPage
