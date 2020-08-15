57dd8c3791bea1961c4c2eed560c978433ee4a12
sergio <sergio_dr@hotmail.com.ar>
2020-08-15 19:33:37 -0300

    styles 2

--- website/src/pages/celular.js
+++ website/src/pages/celular.js
@@ -2,13 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import { Container, Row, Col } from "react-bootstrap"
 import FormCelular from "../components/formCelular"
-import Image from "../components/image";
+import Image from "../components/image"
 import Footer from "../components/footer"
 
 const Celular = () => (
-
   <Layout>
-    <Container >
+    <Container>
       <h1 className="text-center p-3 tituloPer">Informe de Celular</h1>
       <Row>
         <Col md={6} className=" mb-auto mt-5">
@@ -17,58 +16,86 @@ const Celular = () => (
               Contenido Informe Celular
             </h3>
             <Row className="m-auto w-75 ">
-              <Col md={6} xs={6}  >
+              <Col md={6} xs={6}>
                 <p className=" text-left pt-3 pb-1  shadow-h1 ">
-                  -<span className="font-weight-bold font-italic tituloPer">P</span>restador
-                  original
+                  &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    P
+                  </span>
+                  restador original
                 </p>
                 <p className=" text-left pb-1 shadow-h1">
-                  -<span className="font-weight-bold font-italic  tituloPer">P</span>
+                  &#9745;{" "}
+                  <span className="font-weight-bold font-italic  tituloPer">
+                    P
+                  </span>
                   restador actual
                 </p>
                 <p className=" text-left pb-3 shadow-h1 ">
-                  -<span className="font-weight-bold font-italic tituloPer">T</span>itular
+                  &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    T
+                  </span>
+                  itular
                 </p>
                 <p className=" text-left pb-3 shadow-h1">
-                  -<span className="font-weight-bold font-italic tituloPer">C</span>lase
+                  &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    C
+                  </span>
+                  lase
                 </p>
                 <p className=" text-left  pb-1 shadow-h1">
-                  -<span className="font-weight-bold  font-italic tituloPer">D</span>ni
+                  &#9745;{" "}
+                  <span className="font-weight-bold  font-italic tituloPer">
+                    D
+                  </span>
+                  ni
                 </p>
               </Col>
-              <Col md={3} xs={6} className=" mr-md-5">
+              <Col md={4} xs={6} className=" mr-md-5">
                 <p className=" text-left pt-3 pb-1 shadow-h1">
-                  -<span className="font-weight-bold font-italic tituloPer">C</span>uil
+                &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    C
+                  </span>
+                  uil
                 </p>
-                
+
                 <p className=" text-left pb-1 shadow-h1">
-                  -<span className="font-weight-bold font-italic tituloPer">D</span>
+                &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    D
+                  </span>
                   ireccion
                 </p>
                 <p className=" text-left pb-1 shadow-h1">
-                  -<span className="font-weight-bold font-italic tituloPer">L</span>
+                &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    L
+                  </span>
                   ocalidad
                 </p>
                 <p className=" text-left pb-1  shadow-h1 ">
-                  -<span className="font-weight-bold font-italic tituloPer">P</span>
+                  &#9745;{" "}
+                  <span className="font-weight-bold font-italic tituloPer">
+                    P
+                  </span>
                   rovincia
                 </p>
               </Col>
             </Row>
           </div>
         </Col>
-        <Col className=" mb-auto mt-5 ml-5 mr-5 formuPa ">
+        <Col className=" mb-auto mt-5 ml-2 mr-2  formuPa ">
           <h2 className="text-center">Celular</h2>
           <FormCelular />
         </Col>
       </Row>
     </Container>
     <br />
-    <Footer/>
+    <Footer />
   </Layout>
-  
-
- 
 )
 
 export default Celular
