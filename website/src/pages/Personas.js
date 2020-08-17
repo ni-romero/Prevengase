8625e2f79fde3f200d686cc984cd80865fa02e1b
ni-romero <luz.nicole.maria@gmail.com>
2020-08-16 21:53:42 -0300

    detalles form persona

--- website/src/pages/Personas.js
+++ website/src/pages/Personas.js
@@ -22,12 +22,15 @@ const handleInputChange = (event) => {
     // console.log(event.target.value)
     setDatos({
         ...datos,
-        [event.target.name] : event.target.value
-    })
+        [event.target.name] : event.target.value 
+
+
+    }) 
 }
 const enviarDatos = (event) =>{
   // event.preventDefault();
   console.log(datos.nombre + ' ' + datos.email)
+  
 }
   return (
     <>
