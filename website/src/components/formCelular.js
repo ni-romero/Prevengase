abdd3bf6b18f0d99c230631338b20636e4b2c38e
ni-romero <luz.nicole.maria@gmail.com>
2020-08-16 22:27:44 -0300

    validano form  cargando datos en consola

--- website/src/components/formCelular.js
+++ website/src/components/formCelular.js
@@ -7,20 +7,43 @@ import {
   Dropdown,
   DropdownButton,
 } from "react-bootstrap"
+import { useForm } from 'react-hook-form'
 
 const FormCelular = () => {
+
+  const {register, errors, handleSubmit} = useForm();
+
+  const onSubmit = (data, e) => {
+      console.log(data)
+
+      e.target.reset();
+  }
   return (
-    <Form id="formCelular">
+    <Form id="formCelular"onSubmit={handleSubmit(onSubmit)}>
       <Row>
         <Col>
           <Form.Group>
             <Form.Label className="w-100 text-start ">Numero </Form.Label>
             <Form.Control
-              type="text"
+              type="number"
               placeholder=" Ingrese Numero "
               className=" shadow botonEmpresa"
+              name="numero"
+              ref={register({
+                  required: {
+                      value: true, 
+                      message: 'Número es requerido'
+                      }, 
+                  maxLength: {
+                      value: 10, 
+                      message: 'No más de 10 carácteres!'
+                      }
+              })}
             />
-            <Form.Text className="text-muted">Ejemplo 1130587239</Form.Text>
+            <span className="text-danger text-small d-block mb-2">
+          {errors.numero && errors.numero.message}
+            </span>
+            <Form.Text className="text-muted">sin 0 ni 15 ej: 3815666777</Form.Text>
           </Form.Group>
         </Col>
         <Col className="p-0 mr-4">
@@ -39,17 +62,37 @@ const FormCelular = () => {
       </Row>
       <Form.Group>
         <Form.Label className="w-50 text-start "> Email </Form.Label>
-        <Col className=" d-flex m-0 p-0 ">
+        <Col className=" m-0 p-0 ">
           <Form.Control
             type="email"
             placeholder=" Ingrese Email"
             className=" botonEmpresa shadow "
+            name="email"
+            ref={register({
+                required: {
+                    value: true, 
+                    message: 'email es requerido'
+                    }, 
+                maxLength: {
+                    value: 40, 
+                    message: 'No más de 40 carácteres!'
+                    },
+                minLength: {
+                    value: 13, 
+                    message: 'Mínimo 13 carácteres'
+                    }
+            })}
+            
           />
+          <span className="text-danger text-small d-block mb-2">
+          {errors.email && errors.email.message}
+      </span>
         </Col>
         <Form.Text className="text-muted">
-          Ingrese el Email donde que desea recibir el informe.
+          Ingrese el Email donde desea recibir el informe.
         </Form.Text>
         <Button
+          type="submit"
           variant="outline-primary "
           size="lg"
           className="mt-3 shadow botonPers "
